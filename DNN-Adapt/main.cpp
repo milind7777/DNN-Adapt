e6a23ac5fed14090b9a022ebfc48e2482b943230
milind7777 <pmilind1999@gmail.com>
2025-04-02 13:20:50 -0700

    Initial Commit

---
+++ DNN-Adapt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DNN-Adapt
+//
+//  Created by Milind Varma Penumathsa on 4/2/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
