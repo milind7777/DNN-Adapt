8cd2cb5492cbb47f8ca28e0bb62f30a85167bf33
milind7777 <pmilind1999@gmail.com>
2025-06-02 19:03:02 -0700

    fix slo penalty calculation

--- src/Executor.h
+++ src/Executor.h
@@ -219,13 +219,18 @@ public:
             }
         }
 
-        float slo_penalty = 0;
+        float slo_penalty_fail = 0;
+        float slo_pentaly_total = 0;
         for(int i=0;i<m;i++) {
             float slo_fail_total = slo_total[i].first;
             float slo_cnt_total = slo_total[i].first + slo_total[i].second;
-            if(slo_cnt_total > 0) slo_penalty += slo_fail_total / slo_cnt_total;
-            LOG_DEBUG(_logger, "SLO LOGGING: {}, fail: {}, success: {}", id_to_model[i], slo_fail_total, slo_cnt_total);
-        } slo_penalty /= m;
+            slo_penalty_fail += slo_fail_total 
+            slo_penalty_total += slo_cnt_total;
+            LOG_DEBUG(_logger, "SLO LOGGING: {}, fail: {}, total: {}", id_to_model[i], slo_fail_total, slo_cnt_total);
+        } 
+        
+        slo_penalty = 0;
+        if(slo_penalty_total > 0) slo_penalty = (slo_penalty_fail) / (slo_penalty_total);
 
         // float slo_penalty = 0;
         // if((slo_fail_total + slo_success_total) > 0) slo_penalty = slo_fail_total / (slo_fail_total + slo_success_total);
